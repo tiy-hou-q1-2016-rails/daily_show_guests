f147861cf178ddda32f975ecbe150a2b33c9e106
William Durden <williamdurden@Williams-MacBook-Air.local>
2016-02-24 15:35:55 -0600

    finish buttons

--- daily_show_guests/app/models/guest.rb
+++ daily_show_guests/app/models/guest.rb
@@ -1,3 +1,11 @@
 class Guest
-  attr_accessor :name, :year, :occupation, :show_date, :occupation_group
+  attr_accessor :name, :year, :occupation, :show_date, :occupation_group, :name_starter
+
+  def name_starter
+    if self.name.chars.first == '('
+      self.name_starter = ' '
+    else
+      self.name_starter = self.name.chars.first
+    end
+  end
 end
